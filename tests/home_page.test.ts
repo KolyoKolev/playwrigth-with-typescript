84c4bf91d6f92c78d5132cfeeeb1cf20f5016285
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-07-03 18:41:28 +0300

    add new test

--- tests/home_page.test.ts
+++ tests/home_page.test.ts
@@ -1,11 +1,15 @@
 import { chromium, Browser, BrowserContext, Page } from 'playwright';
 import { urls } from '../constants';
 
+const getListOfLinks = (selector: string) => {
+  return page.$$(selector);
+};
+
 let browser: Browser;
 let context: BrowserContext;
 let page: Page;
 
-describe('Test the-internet app', () => {
+describe('Test the-internet app home page', () => {
   beforeEach(async () => {
     browser = await chromium.launch({ headless: false });
     context = await browser.newContext();
@@ -23,5 +27,9 @@ describe('Test the-internet app', () => {
     const pageTitle = await page.title();
     console.log(pageTitle);
     expect(pageTitle).toEqual('The Internet');
+  });
+
+  test('should count the total link on the page', async () => {
+    expect(await getListOfLinks('a')).toHaveLength(46);
   });
 });
