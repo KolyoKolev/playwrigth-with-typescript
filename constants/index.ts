efec70878d43458155e8ef93115cd7e33d670873
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-07-04 08:50:45 +0300

    add constants file

---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+export const urls = {
+  base: 'https://the-internet.herokuapp.com/',
+  elementalSelenium: 'http://elementalselenium.com/',
+};
+
+export const pageTitles = {
+  elementalSeleneium:
+    'Elemental Selenium: Receive a Free, Weekly Tip on Using Selenium like a Pro',
+};
